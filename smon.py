6a0af5e95c068cf4a7b9123791e759b4697854fc
Austin Macdonald <austin@dartmouth.edu>
2024-04-22 12:16:11 -0500

    delete unused

--- smon.py
+++ smon.py
@@ -11,16 +11,6 @@ import shutil
 name = "_smon.out"
 sid = os.getsid(os.getpid())
 
-
-def get_size(start_path='.'):
-    total_size = 0
-    for dirpath, _dirnames, filenames in os.walk(start_path):
-        for f in filenames:
-            fp = os.path.join(dirpath, f)
-            total_size += os.path.getsize(fp)
-    return total_size
-
-
 with open(name, "w") as outfile:
     env = {}
     # put some batch scheduler specific env
