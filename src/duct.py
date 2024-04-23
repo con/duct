bfe72e84411e2b53b7714513d4d643ffa37990c5
Yaroslav Halchenko <debian@onerussian.com>
2024-04-23 14:40:53 -0400

    Initial run of "precommit run --all" and ignoring flake8

--- src/duct.py
+++ src/duct.py
@@ -1,17 +1,16 @@
 #!/usr/bin/env python3
-from collections import defaultdict
-from dataclasses import dataclass, field
 import argparse
 import json
 import os
 import pprint
-import sys
 import shutil
 import subprocess
+import sys
 import time
+from collections import defaultdict
+from dataclasses import dataclass, field
 
 import profilers
-
 
 __version__ = "0.0.1"
 ENV_PREFIXES = ("PBS_", "SLURM_", "OSG")
@@ -19,12 +18,15 @@ ENV_PREFIXES = ("PBS_", "SLURM_", "OSG")
 
 class Report:
     """Top level report"""
+
     def __init__(self, command, session_id):
         self.start_time = time.time()
         self.command = command
         self.session_id = session_id
-        self.system_info = {"uid":  os.environ['USER']}
-        self.env = {k: v for k, v in os.environ.items() if k.startswith(ENV_PREFIXES)},
+        self.system_info = {"uid": os.environ["USER"]}
+        self.env = (
+            {k: v for k, v in os.environ.items() if k.startswith(ENV_PREFIXES)},
+        )
         self.gpu = None
         self.subreports = []
         self.stdout = ""
@@ -33,44 +35,63 @@ class Report:
 
     def get_system_info(self):
         """Gathers system information related to CPU, GPU, memory, and environment variables."""
-        self.system_info["memory_total"] = os.sysconf('SC_PAGE_SIZE') * os.sysconf('SC_PHYS_PAGES')
-        self.system_info["cpu_total"] = os.sysconf('SC_NPROCESSORS_CONF')
+        self.system_info["memory_total"] = os.sysconf("SC_PAGE_SIZE") * os.sysconf(
+            "SC_PHYS_PAGES"
+        )
+        self.system_info["cpu_total"] = os.sysconf("SC_NPROCESSORS_CONF")
 
         # GPU information
         if shutil.which("nvidia-smi"):
             try:
-                gpu_info = subprocess.check_output(["nvidia-smi", "--query-gpu=index,name,pci.bus_id,driver_version,memory.total,compute_mode", "--format=csv"], text=True).strip().split('\n')[1:]
-                self.gpus = [dict(zip(gpu_info[0].split(", "), gpu.split(", "))) for gpu in gpu_info[1:]]
+                gpu_info = (
+                    subprocess.check_output(
+                        [
+                            "nvidia-smi",
+                            "--query-gpu=index,name,pci.bus_id,driver_version,memory.total,compute_mode",
+                            "--format=csv",
+                        ],
+                        text=True,
+                    )
+                    .strip()
+                    .split("\n")[1:]
+                )
+                self.gpus = [
+                    dict(zip(gpu_info[0].split(", "), gpu.split(", ")))
+                    for gpu in gpu_info[1:]
+                ]
             except subprocess.CalledProcessError:
                 self.gpus = "Failed to query GPU info"
 
     def generate_subreport(self, session_id, report_interval, subreport):
         """Monitor and log details about all processes in the given session."""
         elapsed_time = time.time() - self.start_time
-        if elapsed_time >= (subreport.number+1) * report_interval:
+        if elapsed_time >= (subreport.number + 1) * report_interval:
             subreport.elapsed_time = elapsed_time
             self.subreports.append(subreport)
 
-            subreport = SubReport(subreport.number+1)
+            subreport = SubReport(subreport.number + 1)
         # TODO currently clobbers, fix when implementing aggregation.
         subreport.session_data = profilers.monitor_processes(session_id)
         return subreport
 
     def __repr__(self):
-        return json.dumps({
-            "Command": self.command,
-            "System": self.system_info,
-            "ENV": self.env,
-            "GPU": self.gpu,
-            "Subreports": [subreport.serialize() for subreport in self.subreports],
-            "STDOUT": self.stdout,
-            "STDERR": self.stderr,
-        })
+        return json.dumps(
+            {
+                "Command": self.command,
+                "System": self.system_info,
+                "ENV": self.env,
+                "GPU": self.gpu,
+                "Subreports": [subreport.serialize() for subreport in self.subreports],
+                "STDOUT": self.stdout,
+                "STDERR": self.stderr,
+            }
+        )
 
 
 @dataclass
 class SubReport:
     """Group of aggregated statestics on a session"""
+
     number: int = 0
     pids_dummy: list = field(default_factory=lambda: defaultdict(list))
     session_data = None
@@ -86,22 +107,41 @@ class SubReport:
 
 
 def main():
-    """ A wrapper to execute a command, monitor and log the process details. """
-    parser = argparse.ArgumentParser(description="A process wrapper script that monitors the execution of a command.")
-    parser.add_argument('command', help="The command to execute.")
-    parser.add_argument('arguments', nargs='*', help="Arguments for the command.")
-    parser.add_argument('--sample-interval', type=float, default=1.0, help="Interval in seconds between status checks of the running process.")
-    parser.add_argument('--report-interval', type=float, default=60.0, help="Interval in seconds at which to report aggregated data.")
+    """A wrapper to execute a command, monitor and log the process details."""
+    parser = argparse.ArgumentParser(
+        description="A process wrapper script that monitors the execution of a command."
+    )
+    parser.add_argument("command", help="The command to execute.")
+    parser.add_argument("arguments", nargs="*", help="Arguments for the command.")
+    parser.add_argument(
+        "--sample-interval",
+        type=float,
+        default=1.0,
+        help="Interval in seconds between status checks of the running process.",
+    )
+    parser.add_argument(
+        "--report-interval",
+        type=float,
+        default=60.0,
+        help="Interval in seconds at which to report aggregated data.",
+    )
     args = parser.parse_args()
 
     try:
-        process = subprocess.Popen([str(args.command)] + args.arguments.copy(), stdout=subprocess.PIPE, stderr=subprocess.PIPE, preexec_fn=os.setsid)
+        process = subprocess.Popen(
+            [str(args.command)] + args.arguments.copy(),
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            preexec_fn=os.setsid,
+        )
         session_id = os.getsid(process.pid)  # Get session ID of the new process
         report = Report(args.command, session_id)
         subreport = SubReport()
 
         while True:
-            subreport = report.generate_subreport(session_id, args.report_interval, subreport)
+            subreport = report.generate_subreport(
+                session_id, args.report_interval, subreport
+            )
             if process.poll() is not None:  # the process has stopped
                 break
             time.sleep(args.sample_interval)
--- test_script.py
+++ test_script.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 
-import time
-import sys
 import argparse
+import sys
+import time
 
 
 def consume_cpu(duration, load):
-    """ Function to consume CPU proportional to 'load' for 'duration' seconds """
+    """Function to consume CPU proportional to 'load' for 'duration' seconds"""
     end_time = time.time() + duration
     while time.time() < end_time:
         for _ in range(load):
@@ -14,7 +14,7 @@ def consume_cpu(duration, load):
 
 
 def consume_memory(size):
-    """ Function to consume amount of memory specified by 'size' in megabytes """
+    """Function to consume amount of memory specified by 'size' in megabytes"""
     # Create a list of size MB
     bytes_in_mb = 1024 * 1024
     memory = bytearray(size * bytes_in_mb)
@@ -24,14 +24,27 @@ def main(duration, cpu_load, memory_size):
     consume_memory(memory_size)
     print("this is of test of STDERR: ERRRRRRRRRRRRRRR", file=sys.stderr)
     consume_cpu(duration, cpu_load)
-    print(f"Test completed. Consumed {memory_size} MB for {duration} seconds with CPU load factor {cpu_load}.")
+    print(
+        f"Test completed. Consumed {memory_size} MB for {duration} seconds with CPU load factor {cpu_load}."
+    )
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Test script to consume CPU and memory.")
-    parser.add_argument('--duration', type=int, default=60, help="Duration to run the test in seconds.")
-    parser.add_argument('--cpu-load', type=int, default=10000, help="Load factor to simulate CPU usage.")
-    parser.add_argument('--memory-size', type=int, default=10, help="Amount of memory to allocate in MB.")
+    parser = argparse.ArgumentParser(
+        description="Test script to consume CPU and memory."
+    )
+    parser.add_argument(
+        "--duration", type=int, default=60, help="Duration to run the test in seconds."
+    )
+    parser.add_argument(
+        "--cpu-load", type=int, default=10000, help="Load factor to simulate CPU usage."
+    )
+    parser.add_argument(
+        "--memory-size",
+        type=int,
+        default=10,
+        help="Amount of memory to allocate in MB.",
+    )
 
     args = parser.parse_args()
     main(args.duration, args.cpu_load, args.memory_size)
