0de7150e30b7cdabc9e2096c8634e9b2650293db
Austin Macdonald <austin@dartmouth.edu>
2024-04-25 15:06:43 -0500

    Add smoketest

---
+++ smoke-test.sh
@@ -0,0 +1,3 @@
+rm -rf .duct/*
+duct --report-interval 2 --sample-interval 0.5 ./test_script.py -- --duration 6 --cpu-load 50000 --memory-size 50000
+find .duct/ -name '*.json' -exec sh -c 'echo "File: $1)"; cat "$1" | jq' _ {} \; 
