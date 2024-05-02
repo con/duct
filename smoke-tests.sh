bec8c95a1e79285422ca9a993a66b954e381e589
Austin Macdonald <austin@dartmouth.edu>
2024-05-02 08:54:21 -0500

    Add python version matrix

--- smoke-tests.sh
+++ smoke-tests.sh
@@ -1,3 +1,4 @@
+# Smoketests
 rm -rf .duct/*
 duct --report-interval 2 --sample-interval 0.5 ./test_script.py -- --duration 6 --cpu-load 50000 --memory-size 500
 find .duct/ -name '*.json' -exec sh -c 'echo "File: $1)"; cat "$1" | jq' _ {} \;
