9742c5d95a92c31c74499545a8441a37e478d876
John T. Wodder II <git@varonathe.org>
2024-06-10 15:36:49 -0400

    Be smarter about temp paths in tests

--- test/utils.py
+++ test/utils.py
@@ -16,10 +16,10 @@ def assert_files(parent_dir, file_list, exists=True):
     if exists:
         for file_path in file_list:
             assert Path(
-                parent_dir + file_path
+                parent_dir, file_path
             ).exists(), f"Expected file does not exist: {file_path}"
     else:
         for file_path in file_list:
             assert not Path(
-                parent_dir + file_path
+                parent_dir, file_path
             ).exists(), f"Unexpected file should not exist: {file_path}"
