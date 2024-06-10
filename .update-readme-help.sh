7d0fd7a180543f34d7d082b22f2f85ddcf160ca6
Austin Macdonald <austin@dartmouth.edu>
2024-06-10 12:12:43 -0500

    Lint yaml more aggressively

--- .update-readme-help.sh
+++ .update-readme-help.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+set -e
+rm -f .help_content.txt
 duct --help > .help_content.txt
 sed -i '/<\!--- BEGIN HELP -->/,/<\!--- END HELP -->/{//!d;}' README.md
-sed -i '/<\!--- BEGIN HELP -->/r .help_content.txt' README.md
-rm .help_content.txt
+sed -i '/<\!--- BEGIN HELP -->/r .help_content.txt' README.md && rm -f .help_content.txt
