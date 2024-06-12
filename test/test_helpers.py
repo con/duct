417fdfe7fdeb3c982ea85e6d81fe5bb75163f8c2
Austin Macdonald <austin@dartmouth.edu>
2024-06-12 09:17:40 -0500

    Test clobber_or_clear

--- test/test_helpers.py
+++ test/test_helpers.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 from unittest import mock
 import pytest
-from duct.__main__ import ensure_directories
+from duct.__main__ import clobber_or_clear, ensure_directories
 
 
 @pytest.mark.parametrize(
@@ -30,3 +30,32 @@ def test_ensure_directories_with_filepart_and_directory_part(
 ) -> None:
     ensure_directories("nested/dir/file_name")
     mock_mkdir.assert_called_once_with("nested/dir", exist_ok=True)
+
+
+@mock.patch("duct.__main__.Path")
+def test_clobber_or_clear_clear(mock_path: mock.MagicMock) -> None:
+    mock_path.return_value.exists.return_value = False
+    clobber_or_clear("mockpath", clobber=False)
+    mock_path.return_value.unlink.assert_not_called()
+
+
+@mock.patch("duct.__main__.Path")
+def test_clobber_or_clear_clear_clobber(mock_path: mock.MagicMock) -> None:
+    mock_path.return_value.exists.return_value = False
+    clobber_or_clear("mockpath", clobber=True)
+    mock_path.return_value.unlink.assert_not_called()
+
+
+@mock.patch("duct.__main__.Path")
+def test_clobber_or_clear_conflict(mock_path: mock.MagicMock) -> None:
+    mock_path.return_value.exists.return_value = True
+    with pytest.raises(FileExistsError):
+        clobber_or_clear("mockpath", clobber=False)
+    mock_path.return_value.unlink.assert_not_called()
+
+
+@mock.patch("duct.__main__.Path")
+def test_clobber_or_clear_clobber_conflict_clobber(mock_path: mock.MagicMock) -> None:
+    mock_path.return_value.exists.return_value = True
+    clobber_or_clear("mockpath", clobber=True)
+    mock_path.return_value.unlink.assert_called()
