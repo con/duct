3856d6194bca28d4354a24b42023fbe79775449f
Austin Macdonald <austin@dartmouth.edu>
2024-06-16 14:45:59 -0500

    Remove first-run test, which is obsolete

--- test/test_report.py
+++ test/test_report.py
@@ -1,7 +1,5 @@
 from __future__ import annotations
-import subprocess
-from unittest.mock import MagicMock, patch
-from duct.__main__ import ProcessStats, Report, Sample, Suffix
+from duct.__main__ import ProcessStats, Sample
 
 stat0 = ProcessStats(
     pcpu=0.0, pmem=0, rss=0, vsz=0, timestamp="2024-06-11T10:09:37-04:00"
@@ -49,35 +47,3 @@ def test_sample_maxtwo_pids() -> None:
     ex2.add(2, stat1)
     maxes = maxes.max(ex2)
     assert maxes.stats == {1: stat1, 2: stat1}
-
-
-@patch("builtins.open", new_callable=MagicMock)
-@patch("duct.__main__.Sample", spec=Sample)
-@patch("duct.__main__.clobber_or_clear")
-@patch("duct.__main__.subprocess.Popen", spec=subprocess.Popen)
-def test_write_pid_samples_set_first_run_only(
-    mock_popen: MagicMock,
-    mock_clob_or_clear: MagicMock,
-    mock_sample: MagicMock,
-    mock_open: MagicMock,
-    temp_output_dir: str,
-) -> None:
-    report = Report(
-        "mock_cmd",
-        ["mock_arg"],
-        None,
-        temp_output_dir,
-        mock_popen,
-        "mock_dt",
-        clobber=False,
-    )
-    report._sample = mock_sample
-    report._sample.for_json.return_value = {"valid": "dict"}
-    assert report._resource_stats_log_path is None
-    report.write_pid_samples()
-    assert report._resource_stats_log_path is not None
-    # mypy says unreachable which is a lie. (_resource_stats_log_path set as side effect of write_pid_samples)
-    mock_open.assert_called_once_with(f"{temp_output_dir}{Suffix.usage}", "a")  # type: ignore
-    mock_clob_or_clear.reset_mock()
-    report.write_pid_samples()
-    mock_clob_or_clear.assert_not_called()
