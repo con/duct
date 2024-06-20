1db14288958fd6501dc9b11d130c452bd618b7ca
Austin Macdonald <austin@dartmouth.edu>
2024-06-20 15:15:23 -0500

    Add averages tests

--- test/test_report.py
+++ test/test_report.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
-from duct.__main__ import ProcessStats, Sample
+from duct.__main__ import Averages, ProcessStats, Sample
 
 stat0 = ProcessStats(
     pcpu=0.0, pmem=0, rss=0, vsz=0, timestamp="2024-06-11T10:09:37-04:00"
@@ -47,3 +47,43 @@ def test_sample_maxtwo_pids() -> None:
     ex2.add_pid(2, stat1)
     maxes = maxes.max(ex2)
     assert maxes.stats == {1: stat1, 2: stat1}
+
+
+def test_average_no_samples() -> None:
+    averages = Averages()
+    assert averages.num_samples == 0
+    sample = Sample()
+    sample.averages = averages
+    assert "averages" not in sample.for_json()
+
+
+def test_averages_one_sample() -> None:
+    sample = Sample()
+    sample.add_pid(1, stat0)
+    averages = Averages.from_sample(sample)
+    assert averages.rss == sample.total_rss
+    assert averages.vsz == sample.total_vsz
+    assert averages.pmem == sample.total_pmem
+    assert averages.pcpu == sample.total_pcpu
+    assert averages.num_samples == 1
+
+
+def test_averages_two_samples() -> None:
+    sample = Sample()
+    sample.add_pid(1, stat0)
+    averages = Averages.from_sample(sample)
+    sample2 = Sample()
+    sample2.add_pid(2, stat1)
+    averages.update(sample2)
+    assert averages.pcpu == (stat0.pcpu + stat1.pcpu) / 2
+
+
+def test_averages_three_samples() -> None:
+    sample = Sample()
+    sample.add_pid(1, stat0)
+    averages = Averages.from_sample(sample)
+    sample2 = Sample()
+    sample2.add_pid(2, stat1)
+    averages.update(sample2)
+    averages.update(sample2)
+    assert averages.pcpu == (stat0.pcpu + (2 * stat1.pcpu)) / 3
