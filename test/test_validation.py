3af906a6a6c0016a69c4157fcea231a669d45a18
Austin Macdonald <austin@dartmouth.edu>
2024-08-13 16:27:44 -0500

    consolidate and parameterize tests

--- test/test_validation.py
+++ test/test_validation.py
@@ -1,6 +1,6 @@
 import argparse
 import pytest
-from con_duct.__main__ import Arguments, Outputs, RecordTypes
+from con_duct.__main__ import Arguments, Outputs, RecordTypes, assert_num
 
 
 def test_sample_less_than_report_interval() -> None:
@@ -52,3 +52,14 @@ def test_sample_equal_greater_than_report_interval() -> None:
             summary_format="",
             quiet=False,
         )
+
+
+@pytest.mark.parametrize("input_value", [0, 1, 2, -1, 100, 0.001, -1.68])
+def test_assert_num_green(input_value: int) -> None:
+    assert_num(input_value)
+
+
+@pytest.mark.parametrize("input_value", ["hi", "0", "one"])
+def test_assert_num_red(input_value: int) -> None:
+    with pytest.raises(AssertionError):
+        assert_num(input_value)
