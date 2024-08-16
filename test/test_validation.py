3c1244de1459775270679cea3f8ea942cb24eca2
Yaroslav Halchenko <debian@onerussian.com>
2024-08-16 17:22:28 -0400

    RF(TESTS): to avoid duplicated specification of Arguments
    We will just pass into argparse Namespace any kwarg set.
    
    Also fix the testing of silent which was not really testing
    anything since nothing was captured since no logging was enabled

--- test/test_validation.py
+++ test/test_validation.py
@@ -1,59 +1,32 @@
 import argparse
 import pytest
-from con_duct.__main__ import Arguments, Outputs, RecordTypes, assert_num
+from con_duct.__main__ import Arguments, assert_num
 
 
 def test_sample_less_than_report_interval() -> None:
-    args = Arguments(
-        command="fake",
-        command_args=[],
-        output_prefix="fake",
+    args = Arguments.from_argv(
+        ["fake"],
         sample_interval=0.01,
         report_interval=0.1,
-        capture_outputs=Outputs.NONE,
-        outputs=Outputs.ALL,
-        record_types=RecordTypes.SYSTEM_SUMMARY,
-        summary_format="",
-        log_level="INFO",
-        clobber=False,
-        quiet=False,
     )
     assert args.sample_interval <= args.report_interval
 
 
 def test_sample_equal_to_report_interval() -> None:
-    args = Arguments(
-        command="fake",
-        command_args=[],
-        output_prefix="fake",
+    args = Arguments.from_argv(
+        ["fake"],
         sample_interval=0.1,
         report_interval=0.1,
-        capture_outputs=Outputs.NONE,
-        outputs=Outputs.ALL,
-        record_types=RecordTypes.SYSTEM_SUMMARY,
-        clobber=False,
-        summary_format="",
-        log_level="INFO",
-        quiet=False,
     )
     assert args.sample_interval == args.report_interval
 
 
 def test_sample_equal_greater_than_report_interval() -> None:
     with pytest.raises(argparse.ArgumentError):
-        Arguments(
-            command="fake",
-            command_args=[],
-            output_prefix="fake",
+        Arguments.from_argv(
+            ["fake"],
             sample_interval=1.0,
             report_interval=0.1,
-            capture_outputs=Outputs.NONE,
-            outputs=Outputs.ALL,
-            record_types=RecordTypes.SYSTEM_SUMMARY,
-            clobber=False,
-            summary_format="",
-            log_level="INFO",
-            quiet=False,
         )
 
 
