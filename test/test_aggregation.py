0dd8c08a47fc3ca6c6f02f343c1df9761ea6eac1
Austin Macdonald <austin@dartmouth.edu>
2024-09-18 12:06:28 -0500

    Fixup: rm commented pseudo

--- test/test_aggregation.py
+++ test/test_aggregation.py
@@ -35,21 +35,6 @@ stat2 = ProcessStats(
 )
 
 
-# sample1 = S
-#
-#
-# @mock.patch("con_duct.__main__.Report.collect_sample")
-# @mock.patch("con_duct.__main__.LogPaths")
-# def test_aggregation_sanity(
-#     mock_log_paths: mock.MagicMock, mock_sample_collect
-# ) -> None:
-#     mock_sample_collect.side_effect = ["a", "b", "c", "d"]
-#     mock_log_paths.prefix = "mock_prefix"
-#     report = Report("_cmd", [], mock_log_paths, EXECUTION_SUMMARY_FORMAT, clobber=False)
-#     for _ in range(4):
-#         print(report.collect_sample())
-
-
 @mock.patch("con_duct.__main__.LogPaths")
 def test_aggregation_num_samples_increment(mock_log_paths: mock.MagicMock) -> None:
     ex0 = Sample()
