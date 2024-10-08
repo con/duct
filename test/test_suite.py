c143154c0a1ce4accb1721aa469f0cf7b184241a
Austin Macdonald <austin@dartmouth.edu>
2024-10-08 17:22:53 -0500

    Fixup: Adjust existing tests for structure change

--- test/test_suite.py
+++ test/test_suite.py
@@ -3,7 +3,7 @@ from typing import Any
 import unittest
 from unittest.mock import MagicMock, mock_open, patch
 import pytest
-from con_duct import suite
+from con_duct.suite import main, pprint_json
 
 
 class TestSuiteHelpers(unittest.TestCase):
@@ -16,7 +16,7 @@ class TestSuiteHelpers(unittest.TestCase):
             command="invalid", file_path="dummy.json", func=return_non_int
         )
         with pytest.raises(TypeError):
-            suite.execute(args)
+            main.execute(args)
 
 
 class TestPPrint(unittest.TestCase):
@@ -24,12 +24,12 @@ class TestPPrint(unittest.TestCase):
     @patch(
         "builtins.open", new_callable=mock_open, read_data='{"mock_key": "mock_value"}'
     )
-    @patch("con_duct.suite.pprint")
+    @patch("con_duct.suite.pprint_json.pprint")
     def test_pprint_json(self, mock_pprint: MagicMock, mock_open: MagicMock) -> None:
         args = argparse.Namespace(
-            command="pp", file_path="dummy.json", func=suite.pprint_json
+            command="pp", file_path="dummy.json", func=pprint_json.pprint_json
         )
-        assert suite.execute(args) == 0
+        assert main.execute(args) == 0
 
         mock_open.assert_called_with("dummy.json", "r")
         mock_pprint.assert_called_once_with({"mock_key": "mock_value"})
@@ -37,19 +37,19 @@ class TestPPrint(unittest.TestCase):
     @patch("builtins.open", side_effect=FileNotFoundError)
     def test_file_not_found(self, _mock_open: MagicMock) -> None:
         args = argparse.Namespace(
-            command="pp", file_path="dummy.json", func=suite.pprint_json
+            command="pp", file_path="dummy.json", func=pprint_json.pprint_json
         )
-        assert suite.execute(args) == 1
+        assert main.execute(args) == 1
 
     @patch("builtins.open", new_callable=mock_open, read_data='{"invalid": "json"')
-    @patch("con_duct.suite.pprint")
+    @patch("con_duct.suite.pprint_json.pprint")
     def test_pprint_invalid_json(
         self, mock_pprint: MagicMock, mock_open: MagicMock
     ) -> None:
         args = argparse.Namespace(
-            command="pp", file_path="dummy.json", func=suite.pprint_json
+            command="pp", file_path="dummy.json", func=pprint_json.pprint_json
         )
-        assert suite.execute(args) == 1
+        assert main.execute(args) == 1
 
         mock_open.assert_called_with("dummy.json", "r")
         mock_pprint.assert_not_called()
