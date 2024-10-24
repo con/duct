320f0afc5c5fc5bdaa086f696cd81dd41c10c71d
Austin Macdonald <austin@dartmouth.edu>
2024-10-24 13:37:40 -0500

    Remove temporary debug messages

--- test/test_execution.py
+++ test/test_execution.py
@@ -223,8 +223,6 @@ def test_signal_exit(temp_output_dir: str) -> None:
             break
         except subprocess.CalledProcessError as e:
             print(f"Attempt {i} failed with msg: {e}", file=sys.stderr)
-            debug_ps_output = subprocess.check_output("ps auxww", shell=True).decode()
-            print(f"Output of ps aux: {debug_ps_output}", file=sys.stderr)
             sleep(0.1)  # Retry after a short delay
 
     if pid is not None:
