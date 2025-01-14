26653b5b122749bbe203d00e1dd2981fcde145f6
Austin Macdonald <austin@dartmouth.edu>
2025-01-14 09:48:32 -0600

    comment out pstree calls
    Each invocation of pstree adds extra pids, which throws off the pid count

--- test/data/abandoning_parent.sh
+++ test/data/abandoning_parent.sh
@@ -8,7 +8,8 @@ for i in `seq 1 $nchildren`; do
 done
 
 echo "Started $nchildren for $$"
-pstree -c -p "$$"
+# Can be useful when running manually, but commented out so we can count pids in tests
+# pstree -c -p "$$"
 
 echo "Starting one more in subprocess"
 
@@ -16,6 +17,7 @@ echo "Starting one more in subprocess"
 
 jobs
 
-pstree -c -p "$$"
+# Can be useful when running manually, but commented out so we can count pids in tests
+# pstree -c -p "$$"
 echo "waiting"
 wait
